834fa4533a827d5684d70d862d566f2d3f4b4369
pokemonwei <pokemonwei@126.com>
2019-11-20 15:59:32 +0800

    fix bugs for log_name in easy logger

--- easy_logger/include/easylogger.h
+++ easy_logger/include/easylogger.h
@@ -1,13 +1,14 @@
 #ifndef EASY_LOGGER_HEADER
 #define EASY_LOGGER_HEADER
 
+#include<iostream>
 #include<ostream>
 #include<string>
 #include<sstream>
 
 #include "spdlog/async.h" 
 #include "spdlog/sinks/basic_file_sink.h"
-
+#include "spdlog/spdlog.h"
 
 class EasyLogger
 {
@@ -31,18 +32,22 @@ class EasyLogger
 
     explicit EasyLogger(std::string log_name,std::string log_path,LOG_LEVEL level)
     {
-        async_log = spdlog::basic_logger_mt<spdlog::async_factory>(log_name, log_path);
+        async_log = spdlog::get(log_name);
+        if(async_log == NULL) 
+            async_log = spdlog::basic_logger_mt<spdlog::async_factory>(log_name, log_path);
         m_level = level;
     }
     
     
     explicit EasyLogger(std::string file_name,int line_num,std::string log_path,LOG_LEVEL level)
     {
-        std::string log_name = "";
-        log_name += file_name;
-        log_name += " : ";
-        log_name += std::to_string(line_num);
-        async_log = spdlog::basic_logger_mt<spdlog::async_factory>(log_name, log_path);
+        source_location = "";
+        source_location += file_name;
+        source_location += " : ";
+        source_location += std::to_string(line_num);
+        async_log = spdlog::get(source_location);
+        if(async_log == NULL) 
+            async_log = spdlog::basic_logger_mt<spdlog::async_factory>(source_location, log_path);
         m_level = level;
     }
     
@@ -51,6 +56,7 @@ class EasyLogger
     LOG_LEVEL m_level;
     std::ostringstream m_oss;
     std::shared_ptr<spdlog::logger> async_log;
+    std::string source_location;
 };
 
 #define EasyLoggerWithTrace(p,l)             \
