518e04ea3a320ac194137f37e1a8b937de6be110
pokemonwei <pokemonwei@126.com>
2020-06-16 15:17:54 +0800

    update easylogger.h

--- easy_logger/include/easylogger.h
+++ easy_logger/include/easylogger.h
@@ -1,10 +1,22 @@
+/*
+ * @Author: wei
+ * @Date: 2020-06-16 10:26:05
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2020-06-16 15:12:57
+ * @Description: file content
+ * @FilePath: /multi-master-tool/easy_logger/include/easylogger.h
+ */
 #ifndef EASY_LOGGER_HEADER
 #define EASY_LOGGER_HEADER
 
+#include <cstddef>
 #include<iostream>
+#include <memory>
 #include<ostream>
 #include<string>
 #include<sstream>
+
+#include<map>
 
 #include "spdlog/async.h"
 #include "spdlog/sinks/basic_file_sink.h"
@@ -12,89 +24,78 @@
 
 class EasyLogger
 {
-    public:
+	public:
 
-    enum LOG_LEVEL
+	enum LOG_LEVEL
     {
-        debug,
-        info,
-        warn,
-        error,
-    };
+		debug,
+		info,
+		warn,
+		error,
+	};
 
-    virtual ~EasyLogger()
-    {
-        auto s = m_oss.str();
-        if(s.length() <= 0) return;
-        switch (m_level)
+	//EasyLogger()
+
+	static std::string debug_string(std::string file_name,unsigned int line,std::string function_name,std::string action,std::string result)
+	{
+		std::string res = "{";
+		res += "[@file # " + file_name +"],[@line # " + std::to_string(line) + "],[@function # "+ function_name +"],[@action # "+ action + "],[@result # " + result +"]";
+		res += "}";
+		return res;
+	}
+
+	static void logger_record(std::string log_file_name,std::string name,std::string log_value,int m_level,bool will_flush = false)
+	{
+		std::shared_ptr<spdlog::logger> async_log;
+		async_log = spdlog::get(name);
+		if(async_log == NULL)
+		{
+			async_log = spdlog::basic_logger_mt<spdlog::async_factory>(name,log_file_name);
+		}
+//		std::cout << log_value << std::endl;
+		switch (m_level)
         {
         case LOG_LEVEL::info:
-            async_log->info(s);
+			spdlog::set_level(spdlog::level::info);
+            async_log->info(log_value);
             break;
         case LOG_LEVEL::debug:
-            async_log->debug(s);
+			spdlog::set_level(spdlog::level::debug);
+            async_log->debug(log_value);
             break;
         case LOG_LEVEL::warn:
-            async_log->warn(s);
+            async_log->warn(log_value);
             break;
         case LOG_LEVEL::error:
-            async_log->error(s);
+            async_log->error(log_value);
             break;
         default:
+			std::cout << "Unkown Log Level" << std::endl;
             break;
         }
-        if(free_flush)
-        {
-            async_log->flush();
-        }
-    };
 
-    EasyLogger & force_flush()
-    {
-        free_flush = true;
-        return *this;
-    }
+		if(will_flush)
+		{
+			async_log->flush();
+		}
+	}
 
-    template <typename T>
-    EasyLogger &operator<<(const T &rhs) {
-        m_oss << rhs;
-        return (*this);
-    }
-
-    // explicit EasyLogger(std::string log_name,std::string log_path,LOG_LEVEL level)
-    // {
-    //     async_log = spdlog::get(log_name);
-    //     if(async_log == NULL)
-    //         async_log = spdlog::basic_logger_mt<spdlog::async_factory>(log_name, log_path);
-    //     m_level = level;
-    // }
-
-    explicit EasyLogger(std::string file_name,int line_num,std::string log_path,LOG_LEVEL level, bool flush_control = false)
-    {
-        source_location = "";
-        source_location += file_name;
-        source_location += " : ";
-        source_location += std::to_string(line_num);
-        async_log = spdlog::get(source_location);
-        if(async_log == NULL)
-            async_log = spdlog::basic_logger_mt<spdlog::async_factory>(source_location, log_path);
-        m_level = level;
-        free_flush = flush_control;
-    }
-
-    private:
-    LOG_LEVEL m_level;
-    std::ostringstream m_oss;
-    std::shared_ptr<spdlog::logger> async_log;
-    std::string source_location;
-    bool free_flush;
+	//~EasyLogger()
+	private:
+	EasyLogger();
+	~EasyLogger();
 };
 
-#define EasyLoggerWithTrace(p,l)             \
-        EasyLogger(__FILE__, __LINE__,p,l)
+#define EasyStringLog(log_file,name,str,level) \
+	EasyLogger::logger_record(log_file,name,str,level)
 
-#define EasyLoggerWithTrace_FLUSH(p,l)             \
-        EasyLogger(__FILE__, __LINE__,p,l,true)
+#define EasyStringLog_ForceFlush(log_file,name,str,level) \
+	EasyLogger::logger_record(log_file,name,str,level,true)
 
+#define EasyDebugLog(f,n,l,action,res) \
+	EasyStringLog(f, n,EasyLogger::debug_string(__FILE__, __LINE__ , __FUNCTION__,action,res) , l)
+
+#define EasyDebugLog_ForceFlush(f,n,l,file_name,line,action,res) \
+	EasyStringLog_ForceFlush(f, n,EasyLogger::debug_string(__FILE__, __LINE__ , __FUNCTION__,action,res) , l)
 
 #endif // !EASY_LOGGER_HEADER
